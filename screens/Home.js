bda017d3f397020d63eca4549dacfcc5afcabd73
Sattapoom Tulyasuk <s6301012630192@kmutnb.ac.th>
2022-03-19 19:53:50 +0700

    Connect to backend and Display response data

--- screens/Home.js
+++ screens/Home.js
@@ -1,36 +1,25 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import Header from '../components/Header';
 import Todolist from '../components/Todolist';
+import DataService from '../services/service';
 
-const testtodos = [
-  {
-    id: "0",
-    title: "Buy milk",
-    description: "5.00",
-    favor:true
-  },
-  {
-    id: "1",
-    title: "Buy eggfdsssssssssssssssssssssssssssssssssssssssssss",
-    description: "At market.",
-    favor:false
-  },
-  {
-    id: "2",
-    title: "ซื้อผัดไทย",
-    description: "ปากซอย",
-    favor:false
-  },
-]
 
 export default function Home({navigation}) {
-  const [todos, onChangeTodos] = useState(testtodos);
+
+  const [todos, onChangeTodos] = useState();
+  DataService.getAll()
+          .then(response =>{
+              onChangeTodos(response.data);
+              console.log(todos);
+          })
+          .catch(e =>{
+            console.log(e);})
 
   return (
     <View style={styles.container}>
-      <Header todos={testtodos} onChangeTodos={onChangeTodos} navigation={ navigation }/>
+      <Header todos={todos} onChangeTodos={onChangeTodos} navigation={ navigation }/>
       <Todolist todos={todos}/>
     </View>
   );
